321b9e6ae17a5cf423b9f75375c5941bf29ecc06
madueudorio <mariaeduardaeudorio@gmail.com>
2023-09-19 15:26:45 -0300

    atualizando

--- app/Http/Controllers/ServicoController.php
+++ app/Http/Controllers/ServicoController.php
@@ -23,31 +23,83 @@ class ServicoController extends Controller
            ],200);
        }
 
-       public function pesquisarPorNome($nome){
-        $servico = Servico::find($nome);
-        if($servico == null){
-           return response()->json([
-            'status'=> false,
-            'message'=> "Usuário não encontrado"
-           ]);
-        }
+       public function pesquisarPorNome(Request $request){
+        $servicos = Servico::where('nome', 'like', '%'. $request->nome.'%')->get();
+        if(count($servicos)>0){
         return response()->json([
             'status'=> true,
-            'data'=> $servico
+            'data'=> $servicos
+        ]);
+    }
+}
+    
+
+public function pesquisarPorDescricao(Request $request){
+    $servicos = Servico::where('descricao', 'like', '%'. $request->descricao.'%')->get();
+    if(count($servicos)>0){
+    return response()->json([
+        'status'=> true,
+        'data'=> $servicos
+    ]);
+}
+}
+
+
+public function excluir($id){
+    $servico = Servico::find($id);
+
+    if(!isset($servico)){
+        return response()->json([
+            'status'=>false,
+            'message'=> "Serviço não encontrado"
+        ]);
+
+    }
+
+
+    $servico->delete();
+    return response()->json([
+        'status'=>true,
+        'message'=>"Serviço excluído com sucesso"
+    ]);
+
+}
+public function update (Request $request){
+    $servico = Servico::find($request->id);
+
+    if(!isset($servico)){
+        return response()->json([
+            'status'=> false,
+            'message'=> 'Serviço não encontrado'
         ]);
     }
 
-    public function pesquisarPorDescricao($descricao){
-        $servico = Servico::find($descricao);
-        if($servico == null){
-           return response()->json([
-            'status'=> false,
-            'message'=> "Usuário não encontrado"
-           ]);
-        }
+    if (isset($request->nome)){
+        $servico->nome = $request->nome;
+    }
+    if (isset($request->descricao)){
+        $servico->descricao = $request->descricao;
+    }
+    if (isset($request->duracao)){
+        $servico->duracao = $request->duracao;
+    }
+    if (isset($request->preco)){
+        $servico->preco = $request->preco;
+    }
+
+    $servico->update();
+
+    return response()->json([
+        'status'=> true,
+        'message'=> 'Serviço atualizado.'
+    ]);
+}
+
+    public function retornarTodos(){
+        $usuarios = servico::all();
         return response()->json([
             'status'=> true,
-            'data'=> $servico
+            'data'=> $usuarios
         ]);
     }
 
